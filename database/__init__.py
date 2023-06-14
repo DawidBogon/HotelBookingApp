ca6f646f5264fd6e2d374d9bedc6e06437734725
PiotrW-bit <65663442+PiotrW-bit@users.noreply.github.com>
2023-06-14 19:31:25 +0200

    Make res (#15)
    * initial changes
    
    * Fixes on database and room search website
    
    * Bugfix_zwracanych pokoi
    
    * Bugfix wyświetlania pokoi
    
    * Removed random x
    
    ---------
    
    Co-authored-by: DawidBogon <bogon.dawid@gmail.com>

--- database/__init__.py
+++ database/__init__.py
@@ -41,28 +41,27 @@ def createAccessPointTables(db):
         id = db.Column(db.Integer, primary_key=True)
         role = db.Column(db.String(255))
 
-    class Hotel(db.Model):
-        __tablename__ = "hotels"
-        id = db.Column(db.Integer, primary_key=True)
-        name = db.Column(db.String(255))
-        api_endpoint = db.Column(db.String(255))
-        number_of_rooms = db.Column(db.Integer)
-        city = db.Column(db.String(255))
-        address = db.Column(db.String(255))
-        rating = db.Column(db.Float)
-
     class Room(db.Model):
         __tablename__ = "rooms"
         id = db.Column(db.Integer, primary_key=True)
-        hotel = db.Column(db.Integer, db.ForeignKey("hotels.id"))
+        hotel_name = db.Column(db.String(255))
         size = db.Column(db.Integer)
         image = db.Column(db.Text)
         mimetype = db.Column(db.String(255))
         number_of_beds = db.Column(db.Integer)
         additionals = db.Column(db.String(255))
         price = db.Column(db.Float)
+        rating = db.Column(db.Float)
+        api_endpoint = db.Column(db.String(255))
+        city = db.Column(db.String(255))
+        address = db.Column(db.String(255))
 
-    return User, Role, Hotel, Room
+        def return_table(self):
+            return dict(id=self.id, hotel_name=self.hotel_name, size=self.size, image=self.image,
+                        mimetypes=self.mimetype, number_of_beds=self.number_of_beds, additionals=self.additionals,
+                        price=self.price, rating=self.rating, api_endpoint=self.api_endpoint, city=self.city, address=self.address)
+
+    return User, Role, Room
 
 
 def createHotelTables(db):
